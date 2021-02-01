7f42cc519fcbb85fcde23618c3b5232ae99914e4
Christophe VILA <christophe.vila@thalesgroup.com>
2021-02-01 15:39:25 +0100

    Reset restart VM if it was running before its call

--- cmd/gokube/cmd/reset.go
+++ cmd/gokube/cmd/reset.go
@@ -2,11 +2,9 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/gemalto/gokube/pkg/gokube"
 	"github.com/gemalto/gokube/pkg/minikube"
 	"github.com/gemalto/gokube/pkg/virtualbox"
 	"github.com/spf13/cobra"
-	"time"
 )
 
 // resetCmd represents the pause command
@@ -27,19 +25,6 @@ func init() {
 	rootCmd.AddCommand(resetCmd)
 }
 
-func confirmResetCommandExecution() {
-	fmt.Println("WARNING: You cannot reset from a running VM")
-	fmt.Print("Press <CTRL+C> within the next 10s it you have to stop VM or press <ENTER> now to continue...")
-	enter := make(chan bool, 1)
-	go gokube.WaitEnter(enter)
-	select {
-	case <-enter:
-	case <-time.After(10 * time.Second):
-		fmt.Println()
-	}
-	time.Sleep(200 * time.Millisecond)
-}
-
 func resetRun(cmd *cobra.Command, args []string) error {
 	if len(args) > 0 {
 		return cmd.Usage()
@@ -50,11 +35,19 @@ func resetRun(cmd *cobra.Command, args []string) error {
 	}
 	if running {
 		fmt.Println("Stopping minikube VM...")
-		err := minikube.Stop()
+		err = minikube.Stop()
 		if err != nil {
 			return err
 		}
 	}
 	fmt.Println("Resetting minikube VM from snapshot...")
-	return virtualbox.RestoreSnapshot("gokube")
+	err = virtualbox.RestoreSnapshot("gokube")
+	if err != nil {
+		return err
+	}
+	if running {
+		return start()
+	} else {
+		return nil
+	}
 }
